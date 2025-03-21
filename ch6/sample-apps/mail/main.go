549833149ff044190e2f2f2af956827b7e3e7fcb
gumamon <48235604+gumamon@users.noreply.github.com>
2025-03-21 18:55:39 +0900

    wip

--- ch6/sample-apps/mail/main.go
+++ ch6/sample-apps/mail/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
+// verifiedEmail 構造体
+type verifiedEmail struct {
+    address string
+}
+
+// UnverifiedEmail 構造体
+type UnverifiedEmail struct {
+    Address string
+}
+
+// Verify メソッド
+func (u UnverifiedEmail) Verify() *verifiedEmail {
+    return &verifiedEmail{address: u.Address}
+}
+
+// SendPasswordResetEmail メソッド
+func (v verifiedEmail) SendPasswordResetEmail() {
+    fmt.Printf("Sending password reset email to %s\n", v.address)
+}
+
 func main() {
-	fmt.Println("Hello, World from app/mail!")
+	unverified := UnverifiedEmail{Address: "hoge@example.com"}
+	verified := unverified.Verify()
+	verified.SendPasswordResetEmail()
 }
